c0fc0496a0dea3e0aa402767e9acc34fe6547713
sasabeghiu <70911600+sasabeghiu@users.noreply.github.com>
2021-05-25 12:38:40 +0200

    Initial commit

---
+++ ChapeauModel/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChapeauModel
+{
+    public class Table
+    {
+        public int Table_ID { get; set; }
+        public int Table_Number { get; set; } //1-10
+        public int Table_Capacity { get; set; } // how many customerrs can fit on one  table? 5
+        public Enum Table_Availability { get; set; } //reserved, occupied, available
+    }
+}

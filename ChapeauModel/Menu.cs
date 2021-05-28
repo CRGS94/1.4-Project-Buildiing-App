1272f0ce3b9ff5ca6f2c9bebbd3d26b71b9a2067
Pablo Gulias <pabloguliasprats@gmail.com>
2021-05-28 14:00:51 +0200

    Revert "(test) added properties for Menu class"
    This reverts commit 76d55d078f0f453005307736cd13613c39a397c6.

--- ChapeauModel/Menu.cs
+++ ChapeauModel/Menu.cs
@@ -6,12 +6,5 @@ namespace ChapeauModel
 {
     class Menu
     {
-        public int Menu_ID { get; set; }
-
-        public string Menu_Name { get; set; }
-
-        public DateTime Start_Time { get; set; }
-
-        public DateTime End_Time { get; set; }
     }
 }

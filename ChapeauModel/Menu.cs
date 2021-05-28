cfac2f20954b0c85d0995924db939834374817b8
Pablo Gulias <pabloguliasprats@gmail.com>
2021-05-28 14:05:30 +0200

    (test) added Properties to Menu class

--- ChapeauModel/Menu.cs
+++ ChapeauModel/Menu.cs
@@ -6,5 +6,12 @@ namespace ChapeauModel
 {
     class Menu
     {
+        public int Menu_ID { get; set; }
+
+        public string Menu_Name { get; set; }
+
+        public DateTime Start_Time { get; set; }
+
+        public DateTime End_Time { get; set; }
     }
 }
